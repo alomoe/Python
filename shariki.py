3001254cda2cc88dafd104f1fc8d377dd809f85f
alomoe <76548063+alomoe@users.noreply.github.com>
2022-12-17 21:10:55 +0800

    Update shariki.py

--- shariki.py
+++ shariki.py
@@ -1,6 +1,8 @@
 import cv2
 import random
 import numpy as np
+import time
+
 cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
 
 cam = cv2.VideoCapture(0)
@@ -8,7 +10,7 @@ cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
 cam.set(cv2.CAP_PROP_EXPOSURE, -4)
 cam.set(cv2.CAP_PROP_AUTO_WB,0)
 
-color_str=['red','blue','yellow']
+color_str=['blue','yellow']
 
 yellow_lower = np.array([11,180,175])
 yellow_upper = np.array([30,240,245])
@@ -19,55 +21,79 @@ red_upper = np.array([10,250,200])
 blue_lower = np.array([80,230,100])
 blue_upper = np.array([150,255,170])
 
+time_start = 0
+x_start=0
+y_start=0
 random.shuffle(color_str)
 
-while cam.isOpened():  
+def speed(time_start,time_end,x_start,y_start,x,y):
+            length=((x - x_start)**2+(y - y_start)**2)**0.5
+            k = 53 / (2*radius)
+            speed=length*k/(time_end-time_start)
+            return speed
+
+def contours(hsv, lower, upper):
+    mask=cv2.inRange(hsv, lower, upper)
+    contours=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+while cam.isOpened():
     _, frame = cam.read()
     frame = cv2.GaussianBlur(frame, (21,21), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  
-    mask_blue = cv2.inRange(hsv, blue_lower,blue_upper)
-    mask_red = cv2.inRange(hsv, red_lower,red_upper)
-    mask_yellow = cv2.inRange(hsv, yellow_lower,yellow_upper)
 
-    contours_r, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours_b, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours_y, _ = cv2.findContours(mask_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    color=None
+    contours_r, _ = contours(hsv, red_lower, red_upper)
+    contours_b, _ = contours(hsv, blue_lower, blue_upper)
+    contours_y, _ = contours(hsv, yellow_lower, yellow_upper)
+
     posledov={}
 
-    for i in range(0,3):
-        if len(contours_r) > 0:
-            c = max(contours_r, key=cv2.contourArea)
-            (x,y), radius = cv2.minEnclosingCircle(c)
-            if radius > 20:
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 0)
-                color="red"
-                posledov[color]=x
-        if len(contours_b) > 0:
-            c = max(contours_b, key=cv2.contourArea)
-            (x,y), radius = cv2.minEnclosingCircle(c)
-            if radius > 20:
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 0)
-                color="blue"
-                posledov[color]=x
-        if len(contours_y) > 0:
-            c = max(contours_y, key=cv2.contourArea)
-            (x,y), radius = cv2.minEnclosingCircle(c)
-            if radius > 20:
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 0)
-                color="yellow"
-                posledov[color]=x
+    if len(contours_r) > 0:
+        c = max(contours_r, key=cv2.contourArea)
+        (x,y), radius = cv2.minEnclosingCircle(c)
+        if radius > 20:
+               cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 0)
+               color="red"
+               posledov[color]=x
+           
+    if len(contours_b) > 0:
+        c = max(contours_b, key=cv2.contourArea)
+        (x,y), radius = cv2.minEnclosingCircle(c)
+        if radius > 20:
+               cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 0)
+               color="blue"
+               posledov[color]=x
+                
+    if len(contours_y) > 0:
+        c = max(contours_y, key=cv2.contourArea)
+        (x,y), radiusy = cv2.minEnclosingCircle(c)
+        if radiusy > 20:
+              cv2.circle(frame, (int(x), int(y)), int(radiusy), (0,255,255), 0)
+              color="yellow"
+              posledov[color]=x
+
+
     sorted_posledov = sorted(posledov,key=posledov.get)
+
+    if len(sorted_posledov)==1:
+        time_end=time.time()
+        spd=speed(time_start,time_end,x_start,y_start,x,y)
+        x_start=x
+        y_start=y
+        time_start=time_end
+        cv2.putText(frame,f"speed = {spd}", (10,460), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,255,0))
+
     count=0
+    
     for i in range(0,len(sorted_posledov)):
         if sorted_posledov[i]==color_str[i]:
             count+=1
-            
-    if count==3:
+
+    if count==2:
         cv2.putText(frame,f"perfect!", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,255,0))
     else:
-        cv2.putText(frame,f"try again!", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,255,0))
+        cv2.putText(frame,f"make a combination!", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,255,0))
+        
 
     cv2.imshow("Image", frame)
     key = cv2.waitKey(50)
